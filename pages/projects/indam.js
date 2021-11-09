ec3d15bcd92f3794a553ae056f6d33e723352364
MaulanaAdil <kenari222@gmail.com>
2021-11-10 00:31:07 +0700

    add detail of project indam, kopihub

--- pages/projects/indam.js
+++ pages/projects/indam.js
@@ -1,12 +1,46 @@
-import { Badge } from '@chakra-ui/layout'
+import { Badge, Center, Container, Link } from '@chakra-ui/layout'
 import Layout from '../../components/layouts/article'
-import { Title } from '../../components/work'
+import { Title, Meta, WorkImage } from '../../components/work'
+import { Image, List, ListItem } from '@chakra-ui/react'
+import Paragraph from '../../components/paragraph'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const Work = () => (
   <Layout title="Informasi PDAM">
-    <Title>
-      Informasi Pembayaran PDAM <Badge>2021</Badge>
-    </Title>
+    <Container>
+      <Title>
+        Informasi Pembayaran PDAM <Badge>2021</Badge>
+      </Title>
+      <Center>
+        <Image src="/images/projects/campus-project-1.png" alt="Indam" mb={8} />
+      </Center>
+      <Paragraph>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </Paragraph>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Website</Meta>
+          <Link href="http://tugasbesar.maulnad.my.id/">
+            http://tugasbesar.maulnad.my.id/
+            <ExternalLinkIcon mx={4} />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Windows/macOS/Linux/IOS/Android</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>Javascript, JQUERY, SASS</span>
+        </ListItem>
+      </List>
+
+      <WorkImage src="/images/projects/campus-project-1(1).png" alt="Indam" />
+      <WorkImage src="/images/projects/campus-project-1(2).png" alt="Indam" />
+    </Container>
   </Layout>
 )
 
--- pages/projects/kopihub.js
+++ pages/projects/kopihub.js
@@ -2,7 +2,9 @@ import { Center, Container } from '@chakra-ui/layout'
 import { Badge } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
 import Paragraph from '../../components/paragraph'
-import { Title, WorkImage } from '../../components/work'
+import { Title, WorkImage, Meta } from '../../components/work'
+import { Image, List, ListItem, Link } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const Work = () => (
   <Layout title="KopiHub">
@@ -11,7 +13,11 @@ const Work = () => (
         KopiHub <Badge>2020</Badge>
       </Title>
       <Center>
-        <WorkImage src="/images/projects/campus-project-2.png" alt="KopiHub" />
+        <Image
+          src="/images/projects/campus-project-2.png"
+          alt="KopiHub"
+          mb={8}
+        />
       </Center>
       <Paragraph>
         Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -19,6 +25,31 @@ const Work = () => (
         ever since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book.
       </Paragraph>
+
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Website</Meta>
+          <Link href={''}>-</Link>
+          <ExternalLinkIcon mx={4} />
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Windows/macOS/Android/IOS</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>PHP, CSS, JQUERY</span>
+        </ListItem>
+      </List>
+
+      <WorkImage
+        src="/images/projects/campus-project-2(1).png"
+        alt="kopiHUB"
+      ></WorkImage>
+      <WorkImage
+        src="/images/projects/campus-project-2(2).png"
+        alt="kopiHUB"
+      ></WorkImage>
     </Container>
   </Layout>
 )
