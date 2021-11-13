52b9d974c0b099a80a3122a4631d88e4da359216
MaulanaAdil <kenari222@gmail.com>
2021-11-14 01:02:36 +0700

    Add projects detail lovcat.js and moviecatalogue.js

--- pages/projects/lovcat.js
+++ pages/projects/lovcat.js
@@ -1,25 +1,55 @@
-import { Badge, Center } from '@chakra-ui/layout'
+import { Badge, Image, ListItem, List, Link } from '@chakra-ui/react'
+import { Container, Center } from '@chakra-ui/layout'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import Layout from '../../components/layouts/article'
+import { Title, Meta, WorkImage } from '../../components/work'
 import Paragraph from '../../components/paragraph'
-import { Title, WorkImage } from '../../components/work'
 
 const Work = () => (
   <Layout title="Lovcat">
-    <Title>
-      Lov Cat<Badge>2021</Badge>
-    </Title>
-    <Center>
+    <Container>
+      <Title>
+        Lov Cat<Badge>2021</Badge>
+      </Title>
+      <Center>
+        <Image
+          src="/images/projects/collaboration-project-1.png"
+          alt="LovCat"
+          mb={8}
+        />
+      </Center>
+      <Paragraph>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </Paragraph>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Website</Meta>
+          <Link href="#">
+            - <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Windows/macOs/Linux</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>React, SASS</span>
+        </ListItem>
+      </List>
+
       <WorkImage
-        src="/images/projects/collaboration-project-1.png"
+        src="/images/projects/collaboration-project-1(1).png"
         alt="LovCat"
       />
-    </Center>
-    <Paragraph>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry&apos;s standard dummy text ever since
-      the 1500s, when an unknown printer took a galley of type and scrambled it
-      to make a type specimen book.
-    </Paragraph>
+      <WorkImage
+        src="/images/projects/collaboration-project-1(2).png"
+        alt="LovCat"
+      />
+    </Container>
   </Layout>
 )
 
--- pages/projects/moviecatalogue.js
+++ pages/projects/moviecatalogue.js
@@ -1,26 +1,49 @@
-import { Badge, Center } from '@chakra-ui/layout'
+import { Center, Container } from '@chakra-ui/layout'
 import Layout from '../../components/layouts/article'
-import { Title, WorkImage } from '../../components/work'
+import { Title, WorkImage, Meta } from '../../components/work'
 import Paragraph from '../../components/paragraph'
+import { Badge, ListItem, List, Link, Image } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const Work = () => (
   <Layout title="movieCatalogue">
-    <Title>
-      Movie Catalogue <Badge>2021</Badge>
-    </Title>
-    <Center>
-      <WorkImage
-        src="/images/projects/project-3.png"
-        alt="MovieCatalogue"
-        mb={8}
-      />
-    </Center>
-    <Paragraph>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry&apos;s standard dummy text ever since
-      the 1500s, when an unknown printer took a galley of type and scrambled it
-      to make a type specimen book.
-    </Paragraph>
+    <Container>
+      <Title>
+        Movie Catalogue <Badge>2021</Badge>
+      </Title>
+      <Center>
+        <Image
+          src="/images/projects/project-3.png"
+          alt="MovieCatalogue"
+          mb={8}
+        />
+      </Center>
+      <Paragraph>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </Paragraph>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Mobile</Meta>
+          <Link href="#">
+            - <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Android</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>Kotlin, Retrofit, Mockito, MVVM</span>
+        </ListItem>
+      </List>
+
+      <WorkImage src="/images/projects/project-3(1).png" alt="Moviecatalogue" />
+      <WorkImage src="/images/projects/project-3(2).png" alt="Moviecatalogue" />
+    </Container>
   </Layout>
 )
 
