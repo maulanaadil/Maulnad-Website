b82ba6e6bed680ab0e00a0e3fc11d93e62f75486
Maulana Adil <maulanaadil.latif@gmail.com>
2022-04-28 21:04:37 +0700

    add content on post page

--- pages/posts.js
+++ pages/posts.js
@@ -4,6 +4,7 @@ import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
 import androidStudyJams from '../public/images/posts/post-1.jpg'
+import reactTypeScriptBootcamp from '../public/images/posts/post-2.jpg'
 
 const Posts = () => (
   <Layout title="Posts">
@@ -20,7 +21,15 @@ const Posts = () => (
             href="https://www.youtube.com/watch?v=dhPDlxcKbeo"
           >
             Became a speaker and facilitator at the Android Study Jams event.
-            This event was delivered in Bahasa Indonesia 🇮🇩
+            This event was delivered in Bahasa Indonesia 🇮🇩.
+          </GridItem>
+          <GridItem
+            title="React Typescript"
+            thumbnail={reactTypeScriptBootcamp}
+            href="https://www.youtube.com/watch?v=R53DJrazOQU&t=1585s"
+          >
+            Became a moderator at bootcamp React Typescript. This event was
+            delivered in Bahasa Indonesia 🇮🇩.
           </GridItem>
         </SimpleGrid>
       </Section>
